c0dc961bbda5aa67553e95fd6b85f9d4c24769aa
Asanka Thennakoon <asankabandara18879@gmail.com>
2021-08-28 10:53:20 +0530

    add top navigation bar

--- deli_meals/lib/main.dart
+++ deli_meals/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:deli_meals/screens/meal_detail_screen.dart';
+import 'package:deli_meals/screens/tabs_screen.dart';
 
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
@@ -15,11 +16,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Deli Meals',
       theme: ThemeData(
-          primarySwatch: Colors.blue,
-          accentColor: Colors.indigo[50],
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
+        primarySwatch: Colors.blue,
+        accentColor: Colors.indigo[50],
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
               bodyText1: TextStyle(
                 color: Color.fromRGBO(20, 51, 51, 1),
               ),
@@ -30,10 +31,12 @@ class MyApp extends StatelessWidget {
                 fontFamily: 'RobotoCondensed',
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
-              ))),
+              ),
+            ),
+      ),
       initialRoute: '/',
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabsScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
       },
---
+++ deli_meals/lib/screens/tabs_screen.dart
@@ -0,0 +1,40 @@
+import 'package:deli_meals/screens/categories_screen.dart';
+import 'package:deli_meals/screens/favorites_screen.dart';
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({Key? key}) : super(key: key);
+
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      //initialIndex: 1,  //this one is use for to say which screen should start at the intial state.
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Meals'),
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.category),
+                text: "Categories",
+              ),
+              Tab(
+                icon: Icon(Icons.star),
+                text: "Favorits",
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[CategoriesScreen(), FavoritesScreen()],
+        ),
+      ),
+    );
+  }
+}
